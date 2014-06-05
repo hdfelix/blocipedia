a90f980c527ef46cff0969143ce177fc1d1a1831
Hector D. Felix <hdfelix@gmail.com>
2014-06-05 15:56:32 -0700

    Fix typo in app name.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Blocipedia::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-	config.action_mailer.default_url_options = { host: 'hfdfelix-blocipedia.herokuapp.com' }
+	config.action_mailer.default_url_options = { host: 'hdfelix-blocipedia.herokuapp.com' }
 end
